f595da611167866d40b573788845489d3bdf1e1f
Bryce Weiner <bryce@altsystem.io>
2018-09-15 23:07:29 -0700

    Basic assets

--- infiniti/protocol/classes.py
+++ infiniti/protocol/classes.py
@@ -26,12 +26,23 @@ class ObjectStatus(Enum):
     ACTIVE = 3          # Fully confirmed in the blockchain
 
 class InfinitiObject(object):
+
+    @property
+    def object_type(self):
+        return self._object_type
+    
+    @property
+    def protobuf_class(self):
+        return self._protobuf_class
+
     def __init__(self):
         if self._uuid is None:
             self._uuid = uuid.uuid4()
             self._block_height = 0
             self._network = NETWORK
             self._tx_id = ''
+            self._status = ObjectStatus.UNREGISTERED
+            self._fee = params_query(self._network,'Infiniti_fee')
         else:
             self.from_uuid(self._uuid)
 
@@ -45,19 +56,6 @@ class InfinitiObject(object):
         # Check to see if the dealer address has a registered identity
         if self.active():
             pass
-
-    def register(self):
-        """
-        Save the Dealer as an Identity on the blockchain as well as the local database
-
-        The "active" flag is set to True when the Dealer appears in the blockchain
-
-        TODO:blockchain
-
-        Saves to DB only
-        """
-
-        raise NotImplementedError
 
     def uuid(self):
         return self._uuid
@@ -75,28 +73,73 @@ class InfinitiObject(object):
         self._status = ObjectStatus.ACTIVE
         return self._status
 
-    @property
-    def object_type(self):
-        return self._object_type
-    
-    @property
-    def protobuf_class(self):
-        return self._protobuf_class
+    def fee_is_valid(self):
+        """
+            Verify the fee was paid to create the Infiniti message 
+
+            Returns True or False
+        """
+        raise NotImplementedError
+
+    def parse_metadata(self):
+        """
+            Parse the metadata for the object type 
+        """
+        raise NotImplementedError
+
+    def register(self):
+        """
+        Save the Dealer as an Identity on the blockchain as well as the local database
+
+        The "active" flag is set to True when the Dealer appears in the blockchain
+
+        TODO:blockchain
+
+        Saves to DB only
+        """
+        raise NotImplementedError
+
+    def is_valid(self):
+        """
+            Run consensus validation on the object: 
+
+            Returns True or False
+        """
+        raise NotImplementedError
+
 
 class Dealer(InfinitiObject):
+    """
+    A Dealer is an identity object required for Deck spawn operations.
+    """
     object_type = 'dealer'
     protobuf_class = 'DeckSpawn'
 
     def __init__(self,uuid=None):
         self._uuid = uuid
         super(InfinitiObject,self).__init__()
-        if self.block_height == 0: # Impossible, so new object
-            self._name = ''
+        if self.block_height == 0: # Impossible, so must be new object
             self._version = 0
-            self._num_decimals = 8
-            self._issue_mode = DeckSpawn.NONE
-            self.metadata = ''
-            self.fee = 100000
+            self._public_key = ''
+            self._rsa_public_key = ''
+            self._metadata = ''
+            self._creator = ''  # for a dealer object, the creator is the address which generated the Infiniti
+                                # transaction, otherwise it's the uuid of identity object, pubkey must match 
+                                # issuer of the deckspan transaction
+    def creator_is_valid(self):
+        pass
+
+    def fee_is_valid(self):
+        pass
+
+    def parse_metadata(self):
+        pass
+
+    def register(self):
+        pass
+
+    def is_valid(self):
+        return self.creator_is_valid() and self.fee_is_valid(self)
 
     def to_json(self):
         return json.dumps({
@@ -109,4 +152,15 @@ class Dealer(InfinitiObject):
             })
     
 class Deck(object):
+    """
+            self._creator = ''  # for a dealer object, the creator is the address which generated the Infiniti
+                                # transaction, otherwise it's the uuid of identity object, pubkey must match 
+                                # issuer of the deckspan transaction
+            self._stxo = '' # This is the transaction hash of the funding transaction from the blockchain txin
+                            # for the Infiniti transaction which creates the object, which provides the pubkey 
+                            # to validate creator, thereby proving the identity of the dealer is the same 
+                            # public key.  If no such TX hash exists, or if the addresses do not match, the 
+                            # Dealer object is invalid.
+
+    """
     pass
